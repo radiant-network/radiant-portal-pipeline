056612cdc251c4bc817fddad1345d90b4daf1c84
Karl Ritchie <ritchie.karl@gmail.com>
2025-03-28 09:17:08 -0400

    feat: (WIP) Add StarRocks operator and trigger

---
+++ dags/lib/operators/starrocks.py
@@ -0,0 +1,55 @@
+import uuid
+
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.sensors.base import BaseSensorOperator
+from airflow.triggers.base import TaskSuccessEvent
+
+from ..triggers.starrocks import (
+    StarRocksTaskCompleteTrigger,
+)
+
+STARROCKS_TASK_TEMPLATE = "StarRocksSQLExecuteQueryOperator_Task_{uid}"
+
+
+class StarRocksSQLExecuteQueryOperator(SQLExecuteQueryOperator, BaseSensorOperator):
+
+    def __init__(
+        self, submit_task: bool = False, max_query_timeout: int = 10000, *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.submit_task = submit_task
+        self._query_timeout = max_query_timeout
+
+    @staticmethod
+    def _prepare_sql(sql: str, query_timeout: int) -> tuple[str, str]:
+        _task_name = STARROCKS_TASK_TEMPLATE.format(uid=str(uuid.uuid4())[-8:])
+        return (
+            f"""
+            submit /*+set_var(query_timeout={query_timeout})*/ task {_task_name} as
+            {sql}
+         """,
+            _task_name,
+        )
+
+    def execute(self, context):
+        if self.submit_task:
+            self.sql, _task_name = self._prepare_sql(
+                sql=self.sql, query_timeout=self._query_timeout
+            )
+
+            super().execute(context)
+            self.defer(
+                trigger=StarRocksTaskCompleteTrigger(
+                    conn_id=self.conn_id,
+                    task_name=_task_name,
+                    sleep_time=5,
+                ),
+                method_name="_is_complete",
+            )
+        else:
+            return super().execute(context)
+
+    def _is_complete(self, context, event=None) -> None:
+        if not isinstance(event, TaskSuccessEvent):
+            print("Task failed")
+        return
---
+++ dags/lib/triggers/starrocks.py
@@ -0,0 +1,52 @@
+import asyncio
+from typing import Any, AsyncIterator
+
+from airflow.hooks.base import BaseHook
+from airflow.triggers.base import BaseTrigger, TaskSuccessEvent
+
+
+class StarRocksTaskCompleteTrigger(BaseTrigger):
+
+    def __init__(self, conn_id, task_name, sleep_time):
+        super().__init__()
+        self.conn_id = conn_id
+        self.task_name = task_name
+        self._sleep_time = sleep_time
+
+        connection = BaseHook.get_connection(conn_id)
+        self.cursor = connection.get_hook(hook_params={}).get_conn().cursor()
+
+    def serialize(self) -> tuple[str, dict[str, Any]]:
+        print("serializing trigger")
+        return (
+            "lib.triggers.starrocks.StarRocksTaskCompleteTrigger",
+            {
+                "conn_id": self.conn_id,
+                "task_name": self.task_name,
+                "sleep_time": self._sleep_time,
+            },
+        )
+
+    def _get_task_completed(self):
+        self.cursor.execute(
+            f"""
+            SELECT state
+            FROM information_schema.task_runs
+            WHERE task_name = '{self.task_name}'
+            ORDER BY CREATE_TIME DESC
+            LIMIT 1
+            """
+        )
+        result = self.cursor.fetchone()
+        if not result or result[0] == "FAILED":
+            raise Exception(f"Task [{self.task_name}] failed")
+
+        if result[0] != "RUNNING":
+            return True
+
+        return False
+
+    async def run(self) -> AsyncIterator[TaskSuccessEvent]:
+        while not self._get_task_completed():
+            await asyncio.sleep(self._sleep_time)
+        yield TaskSuccessEvent()
