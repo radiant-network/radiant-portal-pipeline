59928e733aa88ed9e34e6cc93f7e75d073e29f91
Karl Ritchie <ritchie.karl@gmail.com>
2025-04-03 08:30:17 -0400

    feat: Add open_data dag file

---
+++ dags/etl_open_data.py
@@ -0,0 +1,93 @@
+from airflow import DAG
+from airflow.models import Variable
+from airflow.models.baseoperator import chain
+from airflow.operators.empty import EmptyOperator
+from airflow.utils.task_group import TaskGroup
+
+from commons.operators.starrocks import (
+    StarRocksSQLExecuteQueryOperator,
+)
+from commons.operators.starrocks import SubmitTaskOptions
+
+
+def group_template(group_id):
+    # Insert hashes
+    insert_hashes = StarRocksSQLExecuteQueryOperator(
+        conn_id="starrocks_conn",
+        task_id=f"insert_hashes_{group_id}",
+        sql=f"./sql/open_data/{group_id}_insert_hashes.sql",
+        database=STARROCKS_DATABASE,
+        submit_task=True,
+        submit_task_options=SubmitTaskOptions(
+            max_query_timeout=3600,
+            poll_interval=30,
+            enable_spill=True,
+            spill_mode="auto",
+        ),
+    )
+
+    # Create table if not exists
+    create_table = StarRocksSQLExecuteQueryOperator(
+        conn_id="starrocks_conn",
+        task_id=f"create_table_{group_id}",
+        sql=f"./sql/open_data/{group_id}_create_table.sql",
+        database=STARROCKS_DATABASE,
+        submit_task=False,
+    )
+
+    # Insert table
+    insert_table = StarRocksSQLExecuteQueryOperator(
+        conn_id="starrocks_conn",
+        task_id=f"insert_table_{group_id}",
+        sql=f"./sql/open_data/{group_id}_insert.sql",
+        database=STARROCKS_DATABASE,
+        submit_task=True,
+        submit_task_options=SubmitTaskOptions(
+            max_query_timeout=3600,
+            poll_interval=30,
+            enable_spill=True,
+            spill_mode="auto",
+        ),
+    )
+    return [insert_hashes, create_table, insert_table]
+
+
+with DAG(
+    dag_id="etl_open_data",
+    schedule_interval=None,
+    catchup=False,
+    tags=["etl", "open_data"],
+) as dag:
+
+    STARROCKS_CONNECTION = Variable.get("STARROCKS_CONNECTION")
+    STARROCKS_DATABASE = Variable.get("STARROCKS_DATABASE")
+
+    start = EmptyOperator(
+        task_id="start",
+    )
+
+    # Create variant_dict table
+    create_variant_dict = StarRocksSQLExecuteQueryOperator(
+        conn_id="starrocks_conn",
+        task_id="create_variant_dict_table",
+        sql="./sql/variant_dict_create_table.sql",
+        database=STARROCKS_DATABASE,
+        submit_task=False,
+    )
+
+    tasks = [start, create_variant_dict]
+    group_ids = [
+        "1000_genomes",
+        "clinvar",
+        "dbnsfp",
+        "gnomad",
+        "spliceai",
+        "topmed_bravo",
+    ]
+    for group in group_ids:
+        with TaskGroup(group_id=f"task_group_{group}"):
+            _tasks = group_template(group_id=f"{group}")
+            for task in _tasks:
+                tasks.append(task)
+
+    chain(*tasks)
