93e13888a471753ecc6fbff4332fdce9ad1b3bac
Karl Ritchie <ritchie.karl@gmail.com>
2025-04-29 13:22:45 -0400

    :construction_worker: test: Refactor starrocks integration tests

--- tests/integration/test_starrocks.py
+++ tests/integration/test_starrocks.py
@@ -4,11 +4,11 @@
 """
 
 
-def test_starrocks_iceberg_state(starrocks_iceberg_catalog, setup_namespace, starrocks_session):
+def test_starrocks_iceberg_state(starrocks_iceberg_catalog, starrocks_session):
     with starrocks_session.cursor() as cursor:
-        cursor.execute(f"SHOW TABLES FROM {starrocks_iceberg_catalog.name}.{setup_namespace}")
-        iceberg_tables = cursor.fetchall()
-        assert len(iceberg_tables) == 3
+        cursor.execute("SHOW CATALOGS")
+        catalogs = cursor.fetchall()
+        assert len(catalogs) == 2
 
 
 def test_starrocks_create_table(starrocks_session):
