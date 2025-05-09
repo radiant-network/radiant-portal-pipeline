11fe1769d091a3563744fa1d9f8547083a61287b
jecos <jcostanza@ferlab.bio>
2025-05-09 00:34:33 -0400

    :bug: fix: SJRA-206 create table issues

--- radiant/dags/sql/open_data/init/hpo_gene_panel_create_table.sql
+++ radiant/dags/sql/open_data/init/hpo_gene_panel_create_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS {{ params.starrocks_hpo_gene_panel }} (
   `symbol` varchar(30) NOT NULL COMMENT "",
   `panel` varchar(250) NOT NULL COMMENT "",
-  `hpo_term_name` varchar(200)>  NOT NULL COMMENT "",
-  `hpo_term_id` varchar(200)>  NOT NULL COMMENT ""
+  `hpo_term_name` varchar(200)  NOT NULL COMMENT "",
+  `hpo_term_id` varchar(200)  NOT NULL COMMENT ""
 )
 ENGINE=OLAP
 DUPLICATE KEY(`symbol`, `panel`)
