2a118e7a12e67afce02ae2a6a0cc8aabc5c4e2e9
Karl Ritchie <ritchie.karl@gmail.com>
2025-05-09 08:34:10 -0400

    :zap: feat: SJRA-171 Add omim_gene_panel mock data resource file for integration tests

--- tests/integration/dags/test_import_part.py
+++ tests/integration/dags/test_import_part.py
@@ -30,7 +30,7 @@ def test_import_part_with_valid_sequencing_experiments(
     radiant_airflow_container.exec(["airflow", "dags", "unpause", vcf_dag_id])
     radiant_airflow_container.exec(["airflow", "dags", "unpause", dag_id])
     radiant_airflow_container.exec(["airflow", "dags", "trigger", dag_id, "--conf", _conf])
-    assert poll_dag_until_success(airflow_container=radiant_airflow_container, dag_id=dag_id, timeout=180)
+    assert poll_dag_until_success(airflow_container=radiant_airflow_container, dag_id=dag_id, timeout=120)
 
     _table_count_mapping = {
         "sequencing_experiments": 1,
@@ -41,7 +41,6 @@ def test_import_part_with_valid_sequencing_experiments(
         "variants": 2,
         "variants_frequencies": 2,
         "variants_partitioned": 2,
-        "variants_lookup": 2,
         "staging_variants": 2,
         "staging_variants_frequencies_part": 2,
     }
@@ -51,3 +50,7 @@ def test_import_part_with_valid_sequencing_experiments(
             cursor.execute(f"SELECT COUNT(1) FROM test_{random_test_id}_{_table}")
             response = cursor.fetchall()
             assert response[0][0] == count, f"Table {_table} has count {response[0][0]}, expected {count}"
+
+        cursor.execute(f"SELECT COUNT(1) FROM test_{random_test_id}_variants_lookup")
+        response = cursor.fetchall()
+        assert response[0][0] > 0, f"Table {_table} has count {response[0][0]}, expected {count}"
