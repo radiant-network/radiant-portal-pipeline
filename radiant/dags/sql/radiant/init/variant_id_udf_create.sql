94d644500e8f1534c8e95ec32cafa2c38e9e832f
jecos <jcostanza@ferlab.bio>
2025-05-13 09:19:04 -0400

    :sparkles: feat: SJRA-225 Create UDF

---
+++ radiant/dags/sql/radiant/init/variant_id_udf_create.sql
@@ -0,0 +1,17 @@
+CREATE
+    GLOBAL FUNCTION GET_VARIANT_ID
+(
+    string,
+    bigint,
+    string,
+    string
+) RETURNS bigint
+    PROPERTIES
+(
+    "symbol" =
+    "org.radiant.VariantIdUDF",
+    "type" =
+    "StarrocksJar",
+    "file" =
+    "https://github.com/radiant-network/radiant-starrocks-udf/releases/download/v1.0.0/radiant-starrocks-udf-1.0.0-jar-with-dependencies.jar"
+);
