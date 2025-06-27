af93477d2c73fca82641c7ecaf1d3c7a1c122b43
Karl Ritchie <ritchie.karl@gmail.com>
2025-06-27 09:18:44 -0400

    :construction_worker: wip: SJRA-317 Update MWAA builder docker

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 set -e
 
+cat /etc/system-release
+
 readonly PLUGINS_DIR="/usr/local/airflow/plugins"
-if [[ "${MWAA_AIRFLOW_COMPONENT}" == "worker" ]]
-then
-    export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PLUGINS_DIR}/lib/"
-    echo "Installing Python dependencies..."
-    unzip -o ${PLUGINS_DIR}/plugins.zip -d ${PLUGINS_DIR}
-    pip install ${PLUGINS_DIR}/wheels/*.whl --find-links ${PLUGINS_DIR}/wheels
-else
-    # We need to install this manually for components that are not workers.
-    # Workers will get this from the wheels directory to speed up the installation.
-    pip install --upgrade apache-airflow-providers-common-sql==1.21.0
-fi
+
+echo "Expanding LD_LIBRARY_PATH..."
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PLUGINS_DIR}/lib/"
+
+echo "Installing Python dependencies..."
+pip install ${PLUGINS_DIR}/wheels/*.whl --find-links ${PLUGINS_DIR}/wheels
