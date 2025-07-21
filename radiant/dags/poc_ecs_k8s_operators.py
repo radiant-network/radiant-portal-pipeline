216d02be0a7f6b374e6235337ceaddc9b33da5de
Karl Ritchie <ritchie.karl@gmail.com>
2025-07-21 13:47:47 -0400

    :construction_worker: wip: SJRA-463 POC ECS Airflow

---
+++ radiant/dags/poc_ecs_k8s_operators.py
@@ -0,0 +1,47 @@
+from airflow import DAG
+
+
+def create_portable_task(task_id):
+    from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+
+    return KubernetesPodOperator(
+        kubernetes_conn_id="kubernetes_conn",
+        task_id=task_id,
+        name="run-on-k8s",
+        namespace="radiant",
+        image="radiant-airflow:latest",
+        cmds=["python", "-m", "pip", "freeze"],
+        get_logs=True,
+        is_delete_operator_pod=True,
+    )
+    # else:
+    #     from airflow.providers.amazon.aws.operators.ecs import ECSOperator
+    #     return ECSOperator(
+    #         task_id=task_id,
+    #         cluster="my-cluster",
+    #         task_definition="my-task-def",
+    #         launch_type="FARGATE",
+    #         overrides={
+    #             "containerOverrides": [{
+    #                 "name": "my-container",
+    #                 "command": ["python", "my_script.py"]
+    #             }]
+    #         },
+    #         network_configuration={
+    #             "awsvpcConfiguration": {
+    #                 "subnets": ["subnet-abc123"],
+    #                 "assignPublicIp": "ENABLED",
+    #                 "securityGroups": ["sg-abc123"]
+    #             }
+    #         },
+    #     )
+
+
+with DAG(
+    dag_id="POC-ecs-k8s-operators",
+    schedule_interval=None,
+    catchup=False,
+    tags=["radiant", "poc", "manual"],
+    dag_display_name="[POC] ECS/K8s Operators",
+) as dag:
+    task = create_portable_task("run_compute")
