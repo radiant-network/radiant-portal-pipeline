c55d2394d3ffa7ab5fcace9ad02a77dca9a968ed
jecos <jcostanza@ferlab.bio>
2025-07-23 06:35:03 -0400

    :bug: fix: SJRA-523 remove duplicate in hpo_gene_panel

--- radiant/dags/sql/open_data/hpo_gene_panel_insert.sql
+++ radiant/dags/sql/open_data/hpo_gene_panel_insert.sql
@@ -1,5 +1,5 @@
 INSERT OVERWRITE {{ params.starrocks_hpo_gene_panel }}
-SELECT symbol,
+SELECT distinct symbol,
        concat(hpo_term_name, '(', hpo_term_id, ')') AS panel,
        hpo_term_name,
        hpo_term_id
