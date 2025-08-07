fcaef79ab71d76e54bd425918204975b9f1e0610
jecos <jcostanza@ferlab.bio>
2025-08-07 15:11:21 -0400

    :white_check_mark: feat: SJRA-578 fix tests

--- tests/unit/dags/test_import_vcf.py
+++ tests/unit/dags/test_import_vcf.py
@@ -9,16 +9,17 @@ def test_dag_is_importable(dag_bag):
 
 def test_dag_has_correct_number_of_tasks(dag_bag):
     dag = dag_bag.get_dag(f"{NAMESPACE}-import-vcf")
-    assert len(dag.tasks) == 4  # get_cases and import_vcf
+    assert len(dag.tasks) == 5  # get_cases and import_vcf
 
 
 def test_dag_has_correct_tasks(dag_bag):
     dag = dag_bag.get_dag(f"{NAMESPACE}-import-vcf")
     task_ids = [task.task_id for task in dag.tasks]
     assert task_ids[0] == "get_cases"
-    assert task_ids[1] == "create_parquet_files"
-    assert task_ids[2] == "merge_commits"
-    assert task_ids[3] == "commit_partitions"
+    assert task_ids[1] == "get_namespace"
+    assert task_ids[2] == "create_parquet_files"
+    assert task_ids[3] == "merge_commits"
+    assert task_ids[4] == "commit_partitions"
 
 
 def test_dag_task_dependencies_are_correct(dag_bag):
