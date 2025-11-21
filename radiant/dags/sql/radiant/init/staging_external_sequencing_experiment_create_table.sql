816b9fc8a606e82a1a5974cf7daffe93bcc60834
kritchie <ritchie.karl@gmail.com>
2025-11-21 09:40:23 -0500

    ✨ feat: SJRA-828 Migrate Task, Document & Sequencing Experiment to data model V2 (#126)

--- radiant/dags/sql/radiant/init/staging_external_sequencing_experiment_create_table.sql
+++ radiant/dags/sql/radiant/init/staging_external_sequencing_experiment_create_table.sql
@@ -1,66 +1,63 @@
 CREATE VIEW IF NOT EXISTS {{ mapping.starrocks_staging_external_sequencing_experiment }} AS
-SELECT
-	case_id,
-	seq_id,
-	task_id,
-	analysis_type,
-	aliquot,
-	patient_id,
-	experimental_strategy,
-	request_priority,
-	MIN(vcf_filepath) AS vcf_filepath,
-    MIN(cnv_vcf_filepath) AS cnv_vcf_filepath,
-	MIN(exomiser_filepath) AS exomiser_filepath,
-	sex,
-	family_role,
-	affected_status,
-	created_at,
-	updated_at
-FROM (
-    SELECT
-        se.case_id AS case_id,
-        se.id AS seq_id,
-        thse.task_id AS task_id,
-        c.case_type_code AS analysis_type,
-        se.aliquot AS aliquot,
-        se.patient_id AS patient_id,
-        exp.experimental_strategy_code AS experimental_strategy,
+with sequencing_experiments AS (
+	SELECT
+	    c.id AS case_id,
+	    c.proband_id,
+	    se.id AS seq_id,
+	    se.sample_id,
+	    se.aliquot,
+	    c.case_type_code AS analysis_type,
+	    se.experimental_strategy_code AS experimental_strategy,
         c.priority_code AS request_priority,
-        CASE WHEN d.format_code = 'vcf' AND d.data_type_code='snv' THEN d.url ELSE NULL END AS vcf_filepath,
-        CASE WHEN d.format_code = 'vcf' AND d.data_type_code='gcnv' THEN d.url ELSE NULL END AS cnv_vcf_filepath,
-        CASE WHEN d.format_code = 'tsv' THEN d.url ELSE NULL END AS exomiser_filepath,
-        p.sex_code AS sex,
-        IF(p.id = c.proband_id, "proband", f.relationship_to_proband_code) AS family_role,
-        IF(p.id = c.proband_id, "affected", f.affected_status_code) AS affected_status,
+	    MAX(CASE WHEN t.task_type_code = 'radiant_germline_annotation' THEN t.id ELSE NULL END) AS task_id,
+	    ANY_VALUE(CASE WHEN d.format_code = 'vcf' AND d.data_type_code='snv' THEN d.url ELSE NULL END) AS vcf_filepath,
+	    ANY_VALUE(CASE WHEN d.format_code = 'vcf' AND d.data_type_code='gcnv' THEN d.url ELSE NULL END) AS cnv_vcf_filepath,
+	    ANY_VALUE(CASE WHEN d.format_code = 'tsv' THEN d.url ELSE NULL END) AS exomiser_filepath,
         se.created_on AS created_at,
         se.updated_on AS updated_at
-    FROM
-        {{ mapping.clinical_sequencing_experiment }} se
-    JOIN {{ mapping.clinical_case }} c ON se.case_id = c.id
-    LEFT JOIN {{ mapping.clinical_experiment }} exp ON exp.id = se.experiment_id
-    LEFT JOIN {{ mapping.clinical_task_has_sequencing_experiment }} thse ON se.id = thse.sequencing_experiment_id
-    LEFT JOIN {{ mapping.clinical_task_has_document }} thd ON thse.task_id = thd.task_id
-    LEFT JOIN {{ mapping.clinical_document }} d ON thd.document_id = d.id
-    LEFT JOIN {{ mapping.clinical_patient }} p ON se.patient_id = p.id
-    LEFT JOIN {{ mapping.clinical_family }} f ON f.family_member_id = p.id
-    WHERE (
-        (d.format_code = 'vcf' AND d.data_type_code = 'snv')
-        OR (d.format_code = 'vcf' AND d.data_type_code = 'gcnv')
-        OR (d.format_code = 'tsv' AND d.data_type_code = 'exomiser' AND d.url LIKE '%variants.tsv')
-    )
-    AND c.status_code in ('in_progress', 'completed')
-) se
-GROUP BY
-    case_id,
-    seq_id,
-    task_id,
-    analysis_type,
-    aliquot,
-    patient_id,
-    experimental_strategy,
-    request_priority,
-    sex,
-    family_role,
-    affected_status,
-    created_at,
-    updated_at
+	FROM
+	    {{ mapping.clinical_sequencing_experiment }} se
+	LEFT JOIN {{ mapping.clinical_task_context }} tctx ON tctx.sequencing_experiment_id = se.id
+	JOIN {{ mapping.clinical_case }} c ON tctx.case_id = c.id
+	LEFT JOIN {{ mapping.clinical_task }} t ON t.id = tctx.task_id
+	LEFT JOIN {{ mapping.clinical_task_has_document }} thd ON thd.task_id = t.id AND thd.type = 'output'
+	LEFT JOIN {{ mapping.clinical_document }} d ON d.id = thd.document_id
+	WHERE (
+		(d.format_code = 'vcf' AND t.task_type_code = 'radiant_germline_annotation')
+		OR (d.format_code = 'vcf' AND t.task_type_code = 'alignment_germline_variant_calling')
+		OR (d.url LIKE '%variants.tsv' AND t.task_type_code = 'exomiser')
+	)
+	AND c.status_code in ('in_progress', 'completed')
+	GROUP BY
+		c.id,
+		c.proband_id,
+		se.id,
+		se.sample_id,
+		se.aliquot,
+		c.case_type_code,
+		se.experimental_strategy_code,
+		c.priority_code,
+		se.created_on,
+		se.updated_on
+)
+SELECT
+	s.case_id,
+	s.seq_id,
+	s.task_id,
+	s.analysis_type,
+	s.aliquot,
+	p.id as patient_id,
+	s.experimental_strategy,
+	s.request_priority,
+	s.vcf_filepath,
+    s.cnv_vcf_filepath,
+	s.exomiser_filepath,
+	p.sex_code AS sex,
+	IF(p.id = s.proband_id, "proband", f.relationship_to_proband_code) AS family_role,
+    IF(p.id = s.proband_id, "affected", f.affected_status_code) AS affected_status,
+	s.created_at,
+	s.updated_at
+FROM sequencing_experiments s
+LEFT JOIN {{ mapping.clinical_sample }} sa ON sa.id = s.sample_id
+LEFT JOIN {{ mapping.clinical_patient }} p ON p.id = sa.patient_id
+LEFT JOIN {{ mapping.clinical_family }} f ON f.family_member_id = p.id
