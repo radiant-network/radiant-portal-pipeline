2bbe61703bc6b0f26393b7e60152a0edaf604204
Celine Pelletier <celine.pelletier.hsj@ssss.gouv.qc.ca>
2026-01-20 15:20:02 -0500

    feat: SJRA-1089 update 1000 genomes to add new columns ac and an

--- radiant/dags/sql/open_data/init/1000_genomes_create_table.sql
+++ radiant/dags/sql/open_data/init/1000_genomes_create_table.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS {{ mapping.starrocks_1000_genomes }} (
     locus_id BIGINT NOT NULL,
-    af DOUBLE
+    af DOUBLE,
+    ac INT(11),
+    an INT(11)
 )
 ENGINE=OLAP
 DISTRIBUTED BY HASH(locus_id) BUCKETS 10
