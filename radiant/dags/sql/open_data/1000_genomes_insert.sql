ebdddf98add2651e2821af666d505ffbcbcef75a
Celine Pelletier <celine.pelletier.hsj@ssss.gouv.qc.ca>
2026-01-20 15:20:02 -0500

    feat: SJRA-1089 update 1000 genomes to add new columns ac and an

--- radiant/dags/sql/open_data/1000_genomes_insert.sql
+++ radiant/dags/sql/open_data/1000_genomes_insert.sql
@@ -1,6 +1,8 @@
 INSERT OVERWRITE {{ mapping.starrocks_1000_genomes }}
 SELECT
     COALESCE(GET_VARIANT_ID(tg.chromosome, tg.start, tg.reference, tg.alternate), v.locus_id) as locus_id,
-    tg.af
+    tg.af,
+    tg.ac,
+    tg.an
 FROM {{ mapping.iceberg_1000_genomes }} tg
 LEFT JOIN {{ mapping.starrocks_variant_lookup }} v ON tg.locus_hash = v.locus_hash;
