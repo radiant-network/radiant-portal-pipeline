eef3792446254f97cc6b08e45e794ee468716ba7
Celine Pelletier <celine.pelletier.hsj@ssss.gouv.qc.ca>
2026-01-20 11:43:19 -0500

    feat: SJRA-1090 add new columns in topmed_bravo table

--- radiant/dags/sql/open_data/topmed_bravo_insert.sql
+++ radiant/dags/sql/open_data/topmed_bravo_insert.sql
@@ -1,6 +1,9 @@
 INSERT OVERWRITE {{ mapping.starrocks_topmed_bravo }}
 SELECT
     COALESCE(GET_VARIANT_ID(t.chromosome, t.start, t.reference, t.alternate), v.locus_id) as locus_id,
-	t.af
+	t.af,
+    t.ac,
+    t.an,
+    t.homozygotes
 FROM {{ mapping.iceberg_topmed_bravo }} t
 LEFT JOIN {{ mapping.starrocks_variant_lookup }} v ON t.locus_hash = v.locus_hash
