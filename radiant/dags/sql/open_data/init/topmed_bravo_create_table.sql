fc2d1ec979b07a700d2b2798129e537b232c0a05
Céline Pelletier <celine.pelletier.hsj@ssss.gouv.qc.ca>
2026-01-21 11:16:10 -0500

    feat: SJRA-1090 add new columns in topmed_bravo table (#134)

--- radiant/dags/sql/open_data/init/topmed_bravo_create_table.sql
+++ radiant/dags/sql/open_data/init/topmed_bravo_create_table.sql
@@ -1,6 +1,9 @@
 CREATE TABLE IF NOT EXISTS {{ mapping.starrocks_topmed_bravo }} (
   `locus_id` bigint(20) NOT NULL COMMENT "",
-  `af` double COMMENT ""
+  `af` double COMMENT "",
+  `ac` INT(11) COMMENT "",
+  `an` INT(11) COMMENT "",
+  `hom` INT(11) COMMENT ""
 )
 ENGINE=OLAP
 DUPLICATE KEY(`locus_id`)
