fb632097ebd2b4e91e9cf5f9307b6a5ddcf750eb
Céline Pelletier <celine.pelletier.hsj@ssss.gouv.qc.ca>
2026-02-03 12:48:32 -0500

    feat: SJRA-1165 add reset seq in seeds script (#138)

--- radiant/tasks/data/radiant_tables.py
+++ radiant/tasks/data/radiant_tables.py
@@ -42,6 +42,9 @@ CLINICAL_MAPPING = {
     "clinical_organization": "organization",
     "clinical_project": "project",
     "clinical_obs_categorical": "obs_categorical",
+    "clinical_panel": "panel",
+    "clinical_obs_string": "obs_string",
+    "clinical_family_history": "family_history",
 }
 
 # --- Iceberg tables
