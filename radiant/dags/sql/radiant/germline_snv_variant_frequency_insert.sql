c8c0ed0bbce169703acb5438c272cd45158d6271
Karl Ritchie <ritchie.karl@gmail.com>
2026-03-03 15:59:45 -0500

    feat: sjra-1252 revert frequencies change

--- radiant/dags/sql/radiant/germline_snv_variant_frequency_insert.sql
+++ radiant/dags/sql/radiant/germline_snv_variant_frequency_insert.sql
@@ -1,5 +1,5 @@
 INSERT
-OVERWRITE {{ mapping.starrocks_snv_variant_frequency }}
+OVERWRITE {{ mapping.starrocks_germline_snv_variant_frequency }}
 WITH patients_total_count AS (
     SELECT SUM(pn_wgs) AS pn_wgs, SUM(pn_wgs_affected) AS pn_wgs_affected, SUM(pn_wgs_not_affected) AS pn_wgs_not_affected, SUM(pn_wxs) AS pn_wxs, SUM(pn_wxs_affected) AS pn_wxs_affected, SUM(pn_wxs_not_affected) AS pn_wxs_not_affected
     from (
@@ -9,7 +9,7 @@ WITH patients_total_count AS (
             ANY_VALUE(pn_wxs) AS pn_wxs,
             ANY_VALUE(pn_wxs_affected) AS pn_wxs_affected,
             ANY_VALUE(pn_wxs_not_affected) AS pn_wxs_not_affected
-        FROM {{ mapping.starrocks_snv_staging_variant_frequency }}
+        FROM {{ mapping.starrocks_germline_snv_staging_variant_frequency }}
         GROUP BY part
     ) t       
 ), 
@@ -21,7 +21,7 @@ freq AS (
                  SUM(pc_wxs)                 AS pc_wxs,
                  SUM(pc_wxs_affected)     AS pc_wxs_affected,
                  SUM(pc_wxs_not_affected) AS pc_wxs_not_affected
-          FROM {{ mapping.starrocks_snv_staging_variant_frequency }}
+          FROM {{ mapping.starrocks_germline_snv_staging_variant_frequency }}
           GROUP BY locus_id
 )
 SELECT locus_id,
