febd64e21cc005e7dc97baddea5ac589cbf7a056
Karl Ritchie <ritchie.karl@gmail.com>
2026-07-02 15:07:17 -0400

    feat: sjra-1455 remove extraneous comments

--- radiant/dags/sql/radiant/snv_consequence_filter_insert_part.sql
+++ radiant/dags/sql/radiant/snv_consequence_filter_insert_part.sql
@@ -4,7 +4,6 @@ SELECT
     c.*
 FROM {{ mapping.starrocks_snv_consequence_filter }} c
 LEFT SEMI JOIN (
-    -- We need to bring all the locus_id(s) from each tenant's occurrences back to the consequence table
     {% for t in tenants %}
     SELECT locus_id
     FROM {{ per_tenant_mapping(t).starrocks_germline_snv_occurrence }}
