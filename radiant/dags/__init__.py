5e32a98a570a7022d83fdbfadcfe68fa3badd57b
Karl Ritchie <ritchie.karl@gmail.com>
2026-09-08 15:43:13 -0400

    feat: sjra-1888 fix formatting & remove unwanted comments

--- radiant/dags/__init__.py
+++ radiant/dags/__init__.py
@@ -21,6 +21,7 @@ IS_AWS = os.environ.get("IS_AWS", "false").lower() == "true"
 RADIANT_LOCK_S3_BUCKET = os.getenv("RADIANT_LOCK_S3_BUCKET", "warehouse")
 
 
+
 def parse_list(env_val):
     return [v.strip() for v in env_val.split(",") if v.strip()]
 
